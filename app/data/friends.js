b34274db7725b29e658cae6a02034385375b30ff
John <johnsonhampson@gmail.com>
2018-07-25 20:36:44 -0400

    friend finder

---
+++ app/data/friends.js
@@ -0,0 +1,164 @@
+var friendsArray = [
+	{
+		name: 'Jen',
+		photo: 'https://randomuser.me/api/portraits/women/67.jpg',
+		scores: [
+			'4',
+			'2',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'3',
+			'5'
+		]
+	},
+	{
+		name: 'Brit',
+		photo: 'https://randomuser.me/api/portraits/women/11.jpg',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Jo',
+		photo: 'https://randomuser.me/api/portraits/women/79.jpg',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Tom',
+		photo: 'https://randomuser.me/api/portraits/men/85.jpg',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Jake',
+		photo: 'https://randomuser.me/api/portraits/men/77.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Mrs. Sara',
+		photo: 'https://randomuser.me/api/portraits/women/65.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Dave',
+		photo: 'https://randomuser.me/api/portraits/men/74.jpg',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Adele',
+		photo: 'https://randomuser.me/api/portraits/women/30.jpg',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Joe',
+		photo: 'https://randomuser.me/api/portraits/men/94.jpg',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Tina',
+		photo: 'https://randomuser.me/api/portraits/women/60.jpg',
+		scores: [
+			'1',
+			'2',
+			'1',
+			'4',
+			'1',
+			'1',
+			'3',
+			'5',
+			'2',
+			'5'
+		]
+	}
+];
+
+module.exports = friendsArray;
