5b03b76922c20ecdb483cd1f4a78378883d83966
John <johnsonhampson@gmail.com>
2018-07-25 22:34:12 -0400

    friend finder

--- server.js
+++ server.js
@@ -3,13 +3,16 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 
+// Sets up the Express App
+// =============================================================
 var app = express();
 var PORT = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, './app/public')));
 
-app.use(bodyParser.json());
+// Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(bodyParser.text());
 
 // Add the application routes
